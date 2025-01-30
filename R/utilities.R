3d492060035f7d713ecbe8fa9674307d4413e3f1
Liz Eidsness <liz.eidsness@agr.gc.ca>
2025-01-30 15:55:14 -0500

    updating to version 0.9
    - added data dictionary to markdown function
    - other cleanup

--- R/utilities.R
+++ R/utilities.R
@@ -10,3 +10,54 @@ loadPackages <- function(package_list) {
     library(p, character.only = TRUE)
   }
 }
+
+#' dictionary_to_markdown
+#' convert a dictionary file to a markdown document 
+#' for pasting into documentation
+#' csv only
+#'
+#' columns expected: COLUMN,TYPE,DESCRIPTION
+#'
+#' @param dictionary_file path to dictionary file
+#' @examples
+#' dictionary_to_markdown("../../my_data_dict.csv")
+#' @export
+#' @import glue
+#' @import knitr
+#' @import tools
+dictionary_to_markdown <- function(dictionary_file) {
+  parse_filename <- function(full_path) {
+    folder <- dirname(full_path)
+    file_with_ext <- basename(full_path)
+    filename <- tools::file_path_sans_ext(file_with_ext)
+    extension <- tools::file_ext(file_with_ext)
+
+    return(list(
+      folder = folder,
+      filename = filename,
+      extension = extension
+    ))
+  }
+
+  description_dict <- smUtilities::fast_read_csv(dictionary_file)
+
+  output <- parse_filename(dictionary_file)
+  out_file <- glue::glue("{output$filename}.md")
+  out_folder <- output$folder
+
+  writeLines(
+    glue::glue("__File:__ {in_file} \\"),
+    file.path(out_folder, out_file)
+  )
+  write(
+    glue::glue("__Data dictionary:__ {dictionary_file}"),
+    sep = "\n", append = TRUE,
+    file = file.path(out_folder, out_file)
+  )
+  md_dict <- knitr::kable(description_dict, "pipe")
+  write(
+    md_dict,
+    sep = "\n", append = TRUE,
+    file = file.path(out_folder, out_file)
+  )
+}
